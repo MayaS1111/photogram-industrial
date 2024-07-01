4e7a0675146098128717d47213a20f7e66d2cfd0
MayaS111 <mayasheriff2018@gmail.com>
2024-07-01 15:55:33 -0500

    added scope to photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -22,7 +22,10 @@
 class Photo < ApplicationRecord
   validates :caption, presence: true
   validates :image, presence: true
-  
+
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
+
   belongs_to :owner, class_name: "User", counter_cache: true
   has_many :comments
 
