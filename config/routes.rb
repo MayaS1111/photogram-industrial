f8187beaa8caa28c8138b2fc77d6ec790adee7a1
MayaS111 <mayasheriff2018@gmail.com>
2024-07-01 16:26:10 -0500

    added validation to like

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root "photos#index"
+  devise_for :users
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos   
-  devise_for :users
-  root "photos#index"
+  
 
   # { :except => [:delete]}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
